ef9832ddefdb209be86ea7aae24713743253f3d9
Server <s@s>
2018-09-22 21:20:54 +0930

    scaled him

---
+++ Tools/blender/scale_animation.py
@@ -0,0 +1,14 @@
+﻿import bpy
+
+y_scale = 0.3
+
+try:
+    for fcu in bpy.context.object.animation_data.action.fcurves:
+        if fcu.data_path.endswith("location"):
+            print("location for: " + fcu.data_path)
+            for kp in fcu.keyframe_points:
+                kp.co.y *= y_scale
+                kp.handle_left.y *= y_scale
+                kp.handle_right.y *= y_scale
+except TypeError:
+    pass
